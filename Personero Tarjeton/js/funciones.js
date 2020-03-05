dd0e888f44d586c043b2d4b69c832dc905a0754e
johnfe66 <johnfe66@gmail.com>
2020-03-05 14:19:26 -0500

    Add files via upload
    Actualizado

---
+++ Personero Tarjeton/js/funciones.js
@@ -0,0 +1,31 @@
+
+
+var santiagoCortes=0;
+var andresLeon=0;
+var dianaDiaz=0;
+var juanMedina=0;
+var heidyLozano=0;
+
+function sumarVotoSC(){
+    santiagoCortes++;
+    
+    console.log('total votos por Santiago Cortés: '+santiagoCortes);
+}
+
+function sumarVotoAL(){
+    andresLeon++;
+    console.log('total votos por Andres Leon: '+andresLeon);
+}
+function sumarVotoDD(){
+    dianaDiaz++;
+    console.log('total votos por Diana Díaz: '+dianaDiaz);
+}
+function sumarVotoJM(){
+    juanMedina++;
+    console.log('total votos por Juan Medina: '+juanMedina);
+}
+function sumarVotoHL(){
+    heidyLozano++;
+
+    console.log('total votos por  Heidy Lozano: '+heidyLozano);
+}
